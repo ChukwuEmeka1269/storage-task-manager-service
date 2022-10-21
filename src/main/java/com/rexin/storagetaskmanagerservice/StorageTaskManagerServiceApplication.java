2df65a8d55e03c1b5e5a278160a0c4dacaed46c8
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-10-21 06:17:39 +0100

    init commit

---
+++ src/main/java/com/rexin/storagetaskmanagerservice/StorageTaskManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rexin.storagetaskmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageTaskManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageTaskManagerServiceApplication.class, args);
+	}
+
+}
